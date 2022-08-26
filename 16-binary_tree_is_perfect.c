9363645b08e4db073a43a37de6652dd7abb1166e
Favvie <favourprecious3@gmail.com>
2022-08-26 05:20:31 +0100

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,44 +1,72 @@
 #include "binary_trees.h"
 
-void traverse(const binary_tree_t *tree, size_t *count);
+int findADepth(const binary_tree_t *node);
+int isPerfectRec(const binary_tree_t *root, int d, int level);
 
 /**
- * binary_tree_leaves - get the number of leaves in a binary tree
+ * binary_tree_is_perfect - checks if tree is a perfect tree
  * @tree: tree to check
  *
- * Return: size of tree
+ * Return: 1 (perfect) 0 (not perfect)
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	int depth;
 
 	if (!tree)
-		return (count);
-	traverse(tree, &count);
-	return (count);
+		return (0);
+
+	depth = findADepth(tree);
+	return (isPerfectRec(tree, depth, 0));
 }
 
 
 /**
- * traverse - goes through a binary tree using in-order traversal
- * @tree: a pointer to the root node of the tree to traverse
- * @count: current count of tree leaves during traversal
+ * findADepth - Returns depth of leftmost leaf.
+ * @node: root node of tree
+ *
+ * Return: depth
  */
 
-void traverse(const binary_tree_t *tree, size_t *count)
+int findADepth(const binary_tree_t *node)
 {
-	if (!tree->left && !tree->right)
+	int d = 0;
+
+	while (node != NULL)
 	{
-		*count += 1;
-		return;
+		d++;
+		node = node->left;
 	}
-	if (tree->left)
-	{
-		traverse(tree->left, count);
-	}
-	if (tree->right)
-	{
-		traverse(tree->right, count);
-	}
+	return (d);
+}
+
+/**
+ * isPerfectRec - recursively checks if a tree is perfect
+ * @root: root node of tree
+ * @d: depth of the tree
+ * @level: current level of node
+ *
+ * Return: 1 (if perfect) 0 (if not)
+ */
+
+int isPerfectRec(const binary_tree_t *root, int d, int level)
+{
+	if (root == NULL)
+		return (0);
+
+	/**
+	 * If leaf node, then its depth must be same as
+	 * depth of all other leaves.
+	 */
+	if (root->left == NULL && root->right == NULL)
+		return (d == level + 1);
+
+	/*If internal node and one child is empty*/
+	if (root->left == NULL || root->right == NULL)
+		return (0);
+
+	/*Left and right subtrees must be perfect.*/
+	return (isPerfectRec(root->left, d, level + 1) &&
+		isPerfectRec(root->right, d, level + 1));
 }
