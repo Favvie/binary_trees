898dcb3c83d4568c09e61a83052fd0b8e6002961
Favvie <favourprecious3@gmail.com>
2022-08-26 05:14:09 +0100

    upload files

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,17 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * stuff
- * @l: more stuff
+ * binary_tree_depth - measure depth of a node
+ * @tree: pointer to node being measured
+ *
+ * Return: depth of node tree
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	size_t depth = 0;
 
-	size_t counter = 0;
-	if (tree->parent)
-		counter = 1 + binary_tree_depth(tree->parent);
-	return (counter);
+	if (tree == NULL)
+		return (depth);
+
+	while (tree->parent)
+	{
+		depth += 1;
+		tree = tree->parent;
+	}
+	return (depth);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,13 +3,13 @@
 void traverse(const binary_tree_t *tree, size_t *count);
 
 /**
- * binary_tree_size - get the size of a binary tree
+ * binary_tree_leaves - get the number of leaves in a binary tree
  * @tree: tree to check
  *
  * Return: size of tree
  */
 
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
@@ -23,16 +23,20 @@ size_t binary_tree_size(const binary_tree_t *tree)
 /**
  * traverse - goes through a binary tree using in-order traversal
  * @tree: a pointer to the root node of the tree to traverse
- * @count: current count of tree size during traversal
+ * @count: current count of tree leaves during traversal
  */
 
 void traverse(const binary_tree_t *tree, size_t *count)
 {
+	if (!tree->left && !tree->right)
+	{
+		*count += 1;
+		return;
+	}
 	if (tree->left)
 	{
 		traverse(tree->left, count);
 	}
-	*count += 1;
 	if (tree->right)
 	{
 		traverse(tree->right, count);
